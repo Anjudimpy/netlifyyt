f0b1214cb040370ba0734d4c0d6831050f36cd41
jeshlyshiny <jeshlyshiny@gmail.com>
2024-07-16 18:46:38 +0530

    cy

--- src/components/OwlCarousel.tsx
+++ src/components/OwlCarousel.tsx
@@ -1,111 +1,96 @@
-import Image from "next/image";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import gel from '../images/ameee/gel.webp';
-import belt from '../images/ameee/safetybelt-1.jpg';
-import { TbHandClick } from "react-icons/tb";
+import React from 'react';
+import Head from 'next/head';
+import Image, { StaticImageData } from 'next/image';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 800 },
-      items: 6,
-      slidesToSlide: 3 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 600, min: 464 },
-      items: 2,
-      slidesToSlide: 1 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
-    }
-  };
+  desktop: {
+    breakpoint: { max: 3000, min: 800 },
+    items: 6,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 600, min: 464 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
-const MyCarousel = () => (
-    <Carousel
+const items = [
+  {
+    src: "/ameee/gel1.jpg",
+    alt: 'Anti-Spatter Gel',
+    description: 'Anti-Spatter Gel for torch nozzle in manual operations.',
+  },
+  {
+    src: "/ameee/goggles-3.jpg",
+    alt: 'Consumables',
+    description: 'Anti-Spatter Gel for torch nozzle in manual operations.',
+  },
+  {
+    src: "/ameee/Calibration-Blocks.jpg",
+    alt: 'Calibration Block',
+    description: 'Calibration blocks ensure accurate measurement and standardization in testing.',
+  },
+  {
+    src: "/ameee/ndtprops.jpg",
+    alt: 'NDT Probes',
+    description: 'Anti-Spatter Gel for torch nozzle in manual operations.',
+  },
+  {
+    src: "/ameee/safety.jpg",
+    alt: 'Safety Accessories',
+    description: 'Anti-Spatter Gel for torch nozzle in manual operations.',
+  },
+  {
+    src: "/ameee/welding.webp",
+    alt: 'Welding Machine',
+    description: 'Anti-Spatter Gel for torch nozzle in manual operations.',
+  },
+];
+
+type CarouselItemProps = {
+  src: string;
+  alt: string;
+  description: string;
+};
+
+const CarouselItem: React.FC<CarouselItemProps> = ({ src, alt, description }) => {
+  return (
+    <div className="w-52 h-52">
+      <div className="relative w-48 h-48 shadow-md shadow-red-400  rounded-full">
+        <Image src={src} alt={alt} width={250} height={250} className="w-48 h-48 owlimg rounded-full bg-cover  bg-no-repeat " />
+        <div className="group absolute top-0 w-48 h-48 rounded-full text-center hover:bg-[#e71c52b9] transition duration-500 cursor-pointer text-amp_blue justify-center border hover:border-blue-950 shadow-md shadow-red-400">
+          <h1 className="text-[15px] py-[70px] group-hover:text-white text-transparent transition duration-500 cursor-pointer absolute font-semibold">
+            {description}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const MyCarousel: React.FC = () => (
+  <Carousel
     responsive={responsive}
     infinite={true}
-    autoPlaySpeed={5000}
-    //  autoPlay={true}
-    itemClass="flex items-center justify-center  "
- 
- 
+    autoPlaySpeed={2000}
+    autoPlay={true}
+    itemClass="flex items-center justify-center"
   >
-    <div className="w-52 h-52 ">
-       <div className="w-48 h-48 shadow-md shadow-red-400   rounded-full">
-        <Image src={gel} alt="" className="w-48 h-48 owlimg rounded-full"/>
-       <div className="group w-48 h-48 rounded-full text-center  hover:bg-red-300 transition duration-500   cursor-pointer text-white justify-center p-2 border hover:border-blue-950  shadow-md shadow-red-400 ">
-           <h1 className="pt-[55px] text-[20px] group-hover:animate-bounce px-[1.2rem] hover:text-gray-700 transition duration-500   cursor-pointer absolute font-bold
-           textShadow ">GEL
-            </h1>
-          
-        </div>
-       </div>
-    </div>
-   
-
-     <div className="w-52 h-52 ">
-       <div className="w-48 h-48 shadow-md shadow-red-400  bg-amp_light  rounded-full">
-       <div className="group w-48 h-48 rounded-full text-center  hover:bg-red-300 transition duration-500   cursor-pointer text-white justify-center p-2 border hover:border-blue-950  shadow-md shadow-red-400 ">
-           <h1 className="pt-[55px] text-[20px] group-hover:animate-bounce  pr-11 hover:text-gray-700 transition duration-500   cursor-pointer absolute font-bold
-           textShadow ">INDUSTRIAL MAINTENANCE   <br></br> PRODUCTS
-           <br></br> 
-            </h1>
-          
-        </div>
-       </div>
-    </div>
-
-    <div className="w-52 h-52 ">
-       <div className="w-48 h-48 shadow-md shadow-red-400  bg-amp_light  rounded-full">
-       <div className="group w-48 h-48 rounded-full text-center  hover:bg-red-300 transition duration-500   cursor-pointer text-white justify-center p-2 border hover:border-blue-950  shadow-md shadow-red-400 ">
-           <h1 className="pt-[69px] text-[20px] group-hover:animate-bounce px-2 hover:text-gray-700 transition duration-500   cursor-pointer absolute font-bold
-           textShadow ">MOULD RELEASE <br></br> AGENTS
-           <br></br>
-            </h1>
-          
-        </div>
-       </div>
-    </div>
-
-     <div className="w-52 h-52 ">
-       <div className="w-48 h-48 shadow-md shadow-red-400  bg-amp_light  rounded-full">
-       <div className="group w-48 h-48 rounded-full text-center  hover:bg-red-300 transition duration-500   cursor-pointer text-white justify-center p-2 border hover:border-blue-950  shadow-md shadow-red-400 ">
-           <h1 className="pt-[60px] pr-12 text-[20px] group-hover:animate-bounce  hover:text-gray-700 transition duration-500   cursor-pointer absolute font-bold
-           textShadow ">SPECIALITY LUBRICANTS<br></br>
-            </h1>
-          
-        </div>
-       </div>
-    </div>
-    <div className="w-52 h-52 ">
-       <div className="w-48 h-48 shadow-md shadow-red-400  bg-amp_light  rounded-full">
-       <div className="group w-48 h-48 rounded-full text-center  hover:bg-red-300 transition duration-500   cursor-pointer text-white justify-center p-2 border hover:border-blue-950  shadow-md shadow-red-400 ">
-           <h1 className="pt-[65px] text-[20px] pr-11 group-hover:animate-bounce  hover:text-gray-700 transition duration-500   cursor-pointer absolute font-bold
-           textShadow "> RTV SILICONE SEALANTS
-            </h1>
-          
-        </div>
-       </div>
-    </div>
-    <div className="w-52 h-52 ">
-       <div className="w-48 h-48 shadow-md shadow-red-400  bg-amp_light  rounded-full">
-       <div className="group w-48 h-48 rounded-full text-center  hover:bg-red-300 transition duration-500   cursor-pointer text-white justify-center p-2 border hover:border-blue-950  shadow-md shadow-red-400 ">
-           <h1 className="pt-[45px] text-[20px] pr-10 group-hover:animate-bounce  hover:text-gray-700 transition duration-500   cursor-pointer absolute font-bold
-           textShadow ">SILICON <br></br>EEMULSIONS, <br></br> FLUIDS AND DEFOAMERS 
-           <br></br> 
-          
-            </h1>
-          
-        </div>
-       </div>
-    </div>
-   
-    
-</Carousel>
+    {items.map((item, index) => (
+      <CarouselItem key={index} src={item.src} alt={item.alt} description={item.description} />
+    ))}
+  </Carousel>
 );
+
+
 
 export default MyCarousel;
