d36a6f4af191d44fdbf99aba3eee2509060e636d
jeshlyshiny <jeshlyshiny@gmail.com>
2024-07-17 18:45:07 +0530

    uu

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -64,17 +64,17 @@ const Description = ({ clickNext, clickPrev, activeImgIndex }: Props) => {
           <div className="leading-relaxed text-white font-medium text-base tracking-wide h-40 italic ">
               {elem.desc}
             </div> 
-            <button className='xl:w-[24%] xs:w-[20%] xs:h-11  btn hover:bg-transparent xl:h-12 pt-1 text-[18px] text-white font-semibold bg-amp_light p-2 border-none rounded-full mx-2  '>
+            <button className='xl:w-[24%] xs:w-[8rem] xs:h-11  btn hover:bg-transparent xl:h-12 pt-1 text-[18px] text-white font-semibold bg-amp_light p-2 border-none rounded-full mx-2  '>
             <div className='absolute btn1 xl:w-[12rem] xs:w-[11rem] xs:h-[11rem] xs:text-[15px] xs:-top-1  xl:-top-2 xl:-left-4 xs:-left-[30px] h-[11rem] pt-[10px]  xl:text-[18px] text-white font-semibold  '>
           Get Started | +</div>
         </button>
-        <button className='xl:w-[24%] xs:w-[20%] xs:h-11  btn hover:bg-transparent xl:h-12 pt-1 text-[18px] text-white font-semibold bg-transparent p-2 border rounded-full mx-2  '>
+        <button className='xl:w-[24%] xs:w-[8rem] xs:h-11  btn hover:bg-transparent xl:h-12 pt-1 text-[18px] text-white font-semibold bg-transparent p-2 border rounded-full mx-2  '>
             <div className='absolute btn1 xl:w-[12rem] xs:w-[11rem] xs:h-[11rem] xs:text-[15px] xs:-top-1  xl:-top-2 xl:-left-4 xs:-left-[30px] h-[11rem] pt-[10px]  xl:text-[18px] text-white font-semibold  '>
           Read More | +
           </div></button>
             </motion.div>
         <div>
-        <div className='flex absolute bottom-[12%]  xl:bottom-[31%] lg:bottom-[31%]  md:bottom-[31%] mdl:bottom-[31%]  xl:left-[29%] lg:left-[38%] mdl:left-[48%] md:left-[54%] left-0   '> 
+        <div className='flex absolute bottom-[65px]  xl:bottom-[31%] lg:bottom-[31%]  md:bottom-[31%] mdl:bottom-[31%]  xl:left-[29%] lg:left-[38%] mdl:left-[48%] md:left-[54%] left-0   '> 
         
          <span className=" vedio-button">
             <TiMediaPlay  className=' absolute bg-[#e03202]  h-10 p-1 w-10 rounded-full text-white' />
